9319c820a4a669f1d5b72291d3c4047e94d4665a
Daiki <ruman.orchid@gmail.com>
2022-06-30 04:17:42 +0900

    fix: format 🐛

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -4,7 +4,7 @@ local cmd = vim.cmd
 local keymap = vim.keymap
 
 -- Global option
-g.mapleader = ' '
+g.mapleader = " "
 
 -- Option --
 opt.number = true
@@ -15,40 +15,40 @@ opt.shiftwidth = 2
 opt.splitright = true
 
 -- Commands --
-cmd [[command! PackerInstall packadd packer.nvim | lua require('plugins').install()]]
-cmd [[command! PackerUpdate packadd packer.nvim | lua require('plugins').update()]]
-cmd [[command! PackerSync packadd packer.nvim | lua require('plugins').sync()]]
-cmd [[command! PackerClean packadd packer.nvim | lua require('plugins').clean()]]
-cmd [[command! PackerCompile packadd packer.nvim | lua require('plugins').compile()]]
+cmd([[command! PackerInstall packadd packer.nvim | lua require('plugins').install()]])
+cmd([[command! PackerUpdate packadd packer.nvim | lua require('plugins').update()]])
+cmd([[command! PackerSync packadd packer.nvim | lua require('plugins').sync()]])
+cmd([[command! PackerClean packadd packer.nvim | lua require('plugins').clean()]])
+cmd([[command! PackerCompile packadd packer.nvim | lua require('plugins').compile()]])
 
 -- Keymaps --
-local opts = {noremap = true, silent = true}
+local opts = { noremap = true, silent = true }
 
 -- Utils
-keymap.set('n', '<C-m>', ':noh<CR>', opts)
-keymap.set('n', '<F12>', '<C-i>', opts)
+keymap.set("n", "<C-m>", ":noh<CR>", opts)
+keymap.set("n", "<F12>", "<C-i>", opts)
 
 -- Emacs like keybindings
-keymap.set('i', '<C-p>', '<Up>', opts)
-keymap.set('i', '<C-n>', '<Down>', opts)
-keymap.set('i', '<C-b>', '<Left>', opts)
-keymap.set('i', '<C-f>', '<Right>', opts)
-keymap.set('i', '<C-a>', '<Home>', opts)
-keymap.set('i', '<C-e>', '<End>', opts)
-keymap.set('i', '<C-d>', '<Del>', opts)
-keymap.set('i', '<C-k>', '<Esc><Right>C', opts)
+keymap.set("i", "<C-p>", "<Up>", opts)
+keymap.set("i", "<C-n>", "<Down>", opts)
+keymap.set("i", "<C-b>", "<Left>", opts)
+keymap.set("i", "<C-f>", "<Right>", opts)
+keymap.set("i", "<C-a>", "<Home>", opts)
+keymap.set("i", "<C-e>", "<End>", opts)
+keymap.set("i", "<C-d>", "<Del>", opts)
+keymap.set("i", "<C-k>", "<Esc><Right>C", opts)
 
 -- Windows control
-keymap.set('n', '<leader>2', ':split<CR>', opts)
-keymap.set('n', '<leader>3', ':vsplit<CR>', opts)
-keymap.set('n', '<leader>h', '<C-w>h', opts)
-keymap.set('n', '<leader>k', '<C-w>k', opts)
-keymap.set('n', '<leader>j', '<C-w>j', opts)
-keymap.set('n', '<leader>l', '<C-w>l', opts)
-keymap.set('n', '<leader>c', '<C-w>c', opts)
-keymap.set('n', '<leader><leader>q', ':q!<CR>', opts)
-keymap.set('n', '<leader><leader>Q', ':qa!<CR>', opts)
+keymap.set("n", "<leader>2", ":split<CR>", opts)
+keymap.set("n", "<leader>3", ":vsplit<CR>", opts)
+keymap.set("n", "<leader>h", "<C-w>h", opts)
+keymap.set("n", "<leader>k", "<C-w>k", opts)
+keymap.set("n", "<leader>j", "<C-w>j", opts)
+keymap.set("n", "<leader>l", "<C-w>l", opts)
+keymap.set("n", "<leader>c", "<C-w>c", opts)
+keymap.set("n", "<leader><leader>q", ":q!<CR>", opts)
+keymap.set("n", "<leader><leader>Q", ":qa!<CR>", opts)
 
 -- QuickFix
-keymap.set('n', '<leader>cn', ':cnext<CR>', opts)
-keymap.set('n', '<leader>cp', ':cprev<CR>', opts)
+keymap.set("n", "<leader>cn", ":cnext<CR>", opts)
+keymap.set("n", "<leader>cp", ":cprev<CR>", opts)
--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -56,9 +56,12 @@ local function init()
 		"Shougo/defx.nvim",
 		config = function()
 			local opts = { noremap = true, silent = true }
-			vim.keymap.set("n", "<leader>f", ":Defx `escape(expand('%:p:h'), ' :')` -search=`expand('%:p')<CR>`", opts)
+			vim.keymap.set("n", "<leader>f", ":Defx `escape(expand('%:p:h'), ' :')` -search=`expand('%:p')`<CR>", opts)
 			local defx_key_settings = function()
-				vim.keymap.set("n", "q", vim.fn["defx#do_action"]("quit"), opts)
+				local onlyBuffer = { noremap = true, silent = true, buffer = true }
+				vim.keymap.set("n", "q", vim.fn["defx#do_action"]("quit"), onlyBuffer)
+				vim.keymap.set("n", "l", vim.fn["defx#do_action"]("open"), onlyBuffer)
+				vim.keymap.set("n", "h", vim.fn["defx#do_action"]("cd", ".."), onlyBuffer)
 			end
 			local id = vim.api.nvim_create_augroup("DefxGroup", {})
 			vim.api.nvim_create_autocmd("FileType", {
