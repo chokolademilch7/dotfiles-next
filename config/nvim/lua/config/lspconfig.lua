37e04a2f5845193e60ffc807a138ee2d0a76a4e6
Daiki <ruman.orchid@gmail.com>
2022-08-31 04:03:21 +0900

    fix: keymap 🐛

--- config/nvim/lua/config/lspconfig.lua
+++ config/nvim/lua/config/lspconfig.lua
@@ -3,19 +3,19 @@ local keymap = vim.keymap
 local lsp = vim.lsp
 local diagnostic = vim.diagnostic
 
-local on_attach = function(client, bufnr)
+local on_attach = function(_, bufnr)
   -- set omnifunc
   -- vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
   -- Mappings.
   local opts = { noremap = true }
-  keymap.set('n', 'C-j', diagnostic.goto_next, opts)
-  keymap.set('n', 'C-k', diagnostic.goto_prev, opts)
+  keymap.set('n', 'gj', diagnostic.goto_next, opts)
+  keymap.set('n', 'gk', diagnostic.goto_prev, opts)
 
   local bufopts = { noremap = true, buffer = bufnr }
   keymap.set('n', 'gD', lsp.buf.declaration, bufopts)
   keymap.set('n', 'gd', lsp.buf.definition, bufopts)
   keymap.set('n', 'K', lsp.buf.hover, bufopts)
-  keymap.set('n', 'gi', lsp.buf.implementation, bufopts)
+  keymap.set('n', 'gI', lsp.buf.implementation, bufopts)
   keymap.set('n', '<C-l>', lsp.buf.signature_help, bufopts)
   keymap.set('n', '<leader>wa', lsp.buf.add_workspace_folder, bufopts)
   keymap.set('n', '<leader>wr', lsp.buf.remove_workspace_folder, bufopts)
@@ -29,7 +29,7 @@ local on_attach = function(client, bufnr)
   keymap.set('i', 'C-i', lsp.buf.completion, bufopts)
   keymap.set('n', 'gs', lsp.buf.workspace_symbol, bufopts)
   keymap.set('n', 'gi', lsp.buf.incoming_calls, bufopts)
-  keymap.set('n', 'gr', lsp.buf.outgoing_calls, bufopts)
+  keymap.set('n', 'go', lsp.buf.outgoing_calls, bufopts)
   keymap.set('n', '<leader>kf', lsp.buf.format, bufopts)
 end
 
