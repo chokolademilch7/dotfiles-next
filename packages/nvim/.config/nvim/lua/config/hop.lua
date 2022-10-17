95fb29276b836bfa3fc5ab57d2986e847c79d646
Daiki <ruman.orchid@gmail.com>
2022-10-17 16:19:01 +0900

    fix: hop keymap 🐛

--- packages/nvim/.config/nvim/lua/config/hop.lua
+++ packages/nvim/.config/nvim/lua/config/hop.lua
@@ -5,8 +5,7 @@ local opts = { noremap = true, silent = true }
 keymap.set('', '<leader><leader>j', '<CMD>HopLine<CR>', opts)
 keymap.set('', '<leader><leader>k', '<CMD>HopLineStart<CR>', opts)
 keymap.set('', '<leader>/', '<CMD>HopPattern<CR>', opts)
-keymap.set('', 'f', '<CMD>HopWord<CR>', opts)
 keymap.set('', 'F', '<CMD>HopWordMW<CR>', opts)
-keymap.set('', '<leader>s', '<CMD>HopChar1CurrentLine<CR>', opts)
+keymap.set('', '<leader>s', '<CMD>HopWord<CR>', opts)
 
 hop.setup({})
