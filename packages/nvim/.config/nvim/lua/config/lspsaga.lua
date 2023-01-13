dd96205bfa394560bb4fb608de41d7602afb750f
Daiki <ruman.orchid@gmail.com>
2023-01-14 01:16:18 +0900

    update: lspsaga

--- packages/nvim/.config/nvim/lua/config/lspsaga.lua
+++ packages/nvim/.config/nvim/lua/config/lspsaga.lua
@@ -4,22 +4,27 @@ local opts = { silent = true }
 
 keymap.set('n', 'ge', '<cmd>Lspsaga rename<CR>', opts)
 keymap.set('n', 'gf', '<cmd>Lspsaga lsp_finder<CR>', opts)
--- keymap.set('n', 'g.', '<cmd>Lspsaga code_action<CR>', opts)
+keymap.set('n', 'g.', '<cmd>Lspsaga code_action<CR>', opts)
 keymap.set('n', 'gp', '<cmd>Lspsaga peek_definition<CR>', opts)
 keymap.set('n', 'go', '<cmd>Lspsaga show_cursor_diagnostics<CR>', opts)
 keymap.set('n', 'K', '<cmd>Lspsaga hover_doc<CR>', opts)
 keymap.set('n', 'gj', '<cmd>Lspsaga diagnostic_jump_next<CR>', opts)
 keymap.set('n', 'gk', '<cmd>Lspsaga diagnostic_jump_prev<CR>', opts)
--- Show line diagnostics
-keymap.set("n", "<leader>cd", "<cmd>Lspsaga show_line_diagnostics<CR>", { silent = true })
--- Show cursor diagnostic
-keymap.set("n", "<leader>cd", "<cmd>Lspsaga show_cursor_diagnostics<CR>", { silent = true })
+keymap.set('n', '<leader>O', '<cmd>Lspsaga outline<CR>', opts)
 
-saga.init_lsp_saga({
-  show_outline = {
+-- Show line diagnostics
+keymap.set('n', '<leader>cd', '<cmd>Lspsaga show_line_diagnostics<CR>', opts)
+-- Show cursor diagnostic
+keymap.set('n', '<leader>cd', '<cmd>Lspsaga show_cursor_diagnostics<CR>', opts)
+
+saga.setup({
+  outline = {
     auto_refresh = true,
   },
-  code_action_lightbulb = {
+  lightbulb = {
     enable = false,
   },
+  symbol_in_winbar = {
+    enable = false,
+  }
 })
