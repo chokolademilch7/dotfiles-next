bee6abb26ee762cd86ee68c120a05ab970778c12
Daiki <ruman.orchid@gmail.com>
2023-07-10 15:14:13 +0900

    update(nvim): agitator custom config

--- packages/nvim/.config/nvim/lua/config/agitator.lua
+++ packages/nvim/.config/nvim/lua/config/agitator.lua
@@ -3,4 +3,25 @@ local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
 keymap.set('n', '<leader>gl', agitator.git_blame_toggle, opts)
+keymap.set('n', '<leader>gl', function()
+  agitator.git_blame_toggle({
+    sidebar_width = 45,
+    formatter = function(r)
+      local summaryMessage
+      if r.author == 'Not Committed Yet' then
+        summaryMessage = 'Nothing'
+      else
+        summaryMessage = r.summary
+      end
+      return string.format(
+        '%02d-%02d-%02d | %s | %s',
+        r.date.year,
+        r.date.month,
+        r.date.day,
+        r.author,
+        summaryMessage
+      )
+    end,
+  })
+end, opts)
 keymap.set('n', '<leader>gt', agitator.git_time_machine, opts)
