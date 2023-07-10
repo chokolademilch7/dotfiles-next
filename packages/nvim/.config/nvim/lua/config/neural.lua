2e655c416029da44451a4df1678ccc65166cc56e
Daiki <ruman.orchid@gmail.com>
2023-07-10 13:46:38 +0900

    add(nvim): neural

---
+++ packages/nvim/.config/nvim/lua/config/neural.lua
@@ -0,0 +1,16 @@
+local neural = require('neural')
+local keymap = vim.keymap
+
+keymap.set('n', 'mm', ':Neural<CR>')
+
+neural.setup({
+  ui = {
+    prompt_icon = ' ⚡'
+  },
+  source = {
+    openai = {
+      api_key = vim.env.OPENAI_API_KEY,
+    }
+  }
+})
+
