422e12bda9ae70faca0003c05da2255066b58f71
Daiki Hirayama <ruman.orchid@gmail.com>
2025-01-27 09:57:13 +0900

    update: flake.nix to modify the description and add flake-utils

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,19 +1,39 @@
 {
-  description = "My development environment";
+  description = "Development environment";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      pkgs = nixpkgs.legacyPackages.x86_64-darwin; # macOSの場合はこちら
-    in {
-      devShells.default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.git
-          pkgs.curl
-        ];
-      };
-    };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        # devShellsをpackagesに変更し、defaultPackageを追加
+        packages.default = pkgs.buildEnv {
+          name = "default-env";
+          paths = with pkgs; [
+            git
+            curl
+            zsh
+          ];
+        };
+
+        # devShellsは残しておく
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            git
+            curl
+            zsh
+          ];
+          
+          shellHook = ''
+            exec zsh
+          '';
+        };
+      }
+    );
 }
